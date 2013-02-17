143f4b64a88216cd54b2edc5524e345c124f03b6
Chaz <cgwennap@FHBZYN1.olin.edu>
2013-02-16 23:05:05 -0500

    fixed minor formatting issues. -Chaz

--- 99 Lisp Problems/problem32.scm
+++ 99 Lisp Problems/problem32.scm
@@ -1,9 +1,9 @@
-;problem33.scm
+;problem32.scm
 ;by Chaz Gwennap, Spring 2013
 
-;solves problem 33 of 99 Lisp problems.
+;solves problem 32 of 99 Lisp problems.
 
-;Problem 33: Determine whether two positive integer numbers are coprime.
+;Problem 32: Determine whether two positive integer numbers are coprime.
 ;Two numbers are coprime if their greatest common divisor equals 1.
 
 ;onevar MUST be greater than twovar!
